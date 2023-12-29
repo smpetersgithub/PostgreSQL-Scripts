fecc33651f24280d996a7cf694c824e45dd2b020
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-12-29 17:27:56 -0600

    Create Table Column Names.sql

---
+++ Table Column Names.sql
@@ -0,0 +1,11 @@
+SELECT  n.nspname AS SchemaName,
+        c.relname AS TableName,
+        a.attname AS ColumnName,
+        pg_catalog.format_type(a.atttypid, a.atttypmod) AS DataType
+FROM    pg_catalog.pg_namespace n INNER JOIN 
+        pg_catalog.pg_class c ON n.oid = c.relnamespace INNER JOIN 
+        pg_catalog.pg_attribute a ON c.oid = a.attrelid
+WHERE   a.attnum > 0  AND 
+        NOT a.attisdropped AND 
+		c.relkind = 'r' -- only include ordinary tables
+ORDER BY SchemaName, TableName, ColumnName;
