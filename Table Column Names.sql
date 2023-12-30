685b41f1aec3c5b9ae863e1ab6d688f6c7c1d5a5
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-12-30 11:28:15 -0600

    Update Table Column Names.sql

--- Table Column Names.sql
+++ Table Column Names.sql
@@ -7,5 +7,5 @@ FROM    pg_catalog.pg_namespace n INNER JOIN
         pg_catalog.pg_attribute a ON c.oid = a.attrelid
 WHERE   a.attnum > 0  AND 
         NOT a.attisdropped AND 
-		c.relkind = 'r' -- only include ordinary tables
+        c.relkind = 'r' -- only include ordinary tables
 ORDER BY SchemaName, TableName, ColumnName;
